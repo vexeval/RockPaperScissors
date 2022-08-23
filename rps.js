678112bc6793fdf1ec864de5402c78b2e1b27997
Matt <vexeval@gmail.com>
2022-08-23 18:51:28 -0400

    Base code structure implemented

---
+++ rps.js
@@ -0,0 +1,41 @@
+function playRound(playerSelection, computerSelection) {
+    
+}
+   
+function getComputerChoice() {
+    choiceNum = Math.floor(Math.random() * 4)
+
+    switch(choiceNum)
+    {
+        case 1:
+            result = 'Rock'
+            break;
+        case 2:
+            result = 'Paper'
+            break;
+        case 3:
+            result = 'Scissors'
+            break;
+        default:
+            console.error("Choice not in range");
+            break;
+    }
+    console.log(`Computer chose ${result}`)
+    return result;
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Input Decision:\n\nRock\nPaper\nScissors\n\nInputting nothing stops the game.");
+
+        if (playerSelection = null) return console.log("Game Exited");
+
+        console.log(`Round ${i} of 5`);     
+
+        playRound(playerSelection, getComputerChoice());
+        
+    }
+    
+}
+
+  
