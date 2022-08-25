d887896e24143689b7a20ed19f04b0e4126c9a71
Matt <vexeval@gmail.com>
2022-08-25 18:53:16 -0400

    Base functionality for playing
    Co-authored-by: NehemyThachaeden

--- rps.js
+++ rps.js
@@ -1,26 +1,40 @@
 function playRound(playerSelection, computerSelection) {
-    
+    console.log(`Player Selection: ${playerSelection}, Computer Selection: ${computerSelection}`)
+
+    if (playerSelection == computerSelection)
+    {
+        alert("Its a tie!")
+    }
+    if (playerSelection == "rock" && computerSelection == "scissors")
+    {
+        alert("Player won!")
+    }
+    else
+    {
+        alert("Unknown")
+    }
 }
    
 function getComputerChoice() {
-    choiceNum = Math.floor(Math.random() * 4)
+    choiceNum = Math.floor(Math.random() * 3)
 
     switch(choiceNum)
     {
+        case 0:
+            result = 'rock'
+            break;
         case 1:
-            result = 'Rock'
+            result = 'paper'
             break;
         case 2:
-            result = 'Paper'
-            break;
-        case 3:
-            result = 'Scissors'
+            result = 'scissors'
             break;
         default:
-            console.error("RPT- Choice not in range");
+            console.log("-- Choice not in range");
             break;
     }
-    console.log(`Computer chose ${result}`)
+    // console.log(`Computer chose ${result}`)
+    
     return result;
 }
 
@@ -28,11 +42,12 @@ function game() {
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt("Input Decision:\n\nRock\nPaper\nScissors\n\nInputting nothing stops the game.");
 
-        if (playerSelection = null) return console.log("Game Exited");
+        if (playerSelection == null) return console.log("Game Exited");
 
         console.log(`Round ${i} of 5`);     
 
-        playRound(playerSelection, getComputerChoice());
+        playRound(playerSelection.toLowerCase(), getComputerChoice());
+
         
     }
     
